e11780928d60bd285312bdfcc90980e7ad17eafa
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-14 12:14:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 document.getElementById("dodger");
 
 dodger.style.backgroundColor = "#FF69B4";
-dodger.style.bottom = "100px";
+dodger.style.bottom = "0px";
 
 console.log(dodger.style.left);
 
 console.log(dodger.style.bottom);
+
+document.addEventListener("keydown")
 

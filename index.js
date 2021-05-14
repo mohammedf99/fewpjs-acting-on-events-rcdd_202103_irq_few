6cf968f71e719c60f4fb317d37bbf2a8ead5c1c3
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-14 12:30:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px";
+
+console.log(dodger.style.left);
+
+console.log(dodger.style.bottom);
+
+document.addEventListener("keydown", (e) => {
+  console.log(e.key);
+  
+  if(e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }else if(e.key === "ArrowRight") {
+    moveDodgerRight()
+  }
+})
+
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+

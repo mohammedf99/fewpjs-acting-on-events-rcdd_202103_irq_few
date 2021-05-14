937610b4147bdc08946571e2e6231319ebd0bee8
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-14 12:21:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,10 +18,11 @@ document.addEventListener("keydown", (e) => {
 
 
 function moveDodgerLeft() {
-    let leftNumber = dodger.style.left.replace("px", "");
-    let left = parseInt(leftNumber, 10);
-    if (left > 0) {
-      dodger.style.left = `${left - 1}`;
-    }
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
 }
 

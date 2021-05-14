a7446582100c61202a6d854ac35567a493f23dd0
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-14 12:30:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,9 +28,12 @@ function moveDodgerLeft() {
   }
 }
 
-function moverDodgerRight() {
-  let rightNumbers = dodger.style.right.replace("px", "");
-  let right = parseInt(rightNumbers, 10);
-  dodger.style.right = `${right + 1}px`;
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
 }
 
